d40fe3a93e5f384d41ea3739a19fd159331389e6
jontehruss <johann.russeau@protonmail.com>
2024-11-16 19:13:52 +0100

    start user login/signup routes

--- BACK/app.js
+++ BACK/app.js
@@ -40,19 +40,55 @@ app.use((req, res, next) => {
 });
 
 
-// Signin - cet endpoint récupère uniquement les requêtes post avec app.post
-app.post('/api/auth/login', (req, res, next) => {
-    // créer un instance du modèle user
+app.post('/api/auth/signup', (req, res, next) => {
+    // Créer un utilisateur
     const user = new User({
         email: req.body.email,
         password: req.body.password
-    });
+    })
 
-    // résoudre la requête
-    res.status(201).json({
-        message: 'Login recieved !'
-    });
+    // méthode save() pour enregistrer en base de données
+    user.save()
+        .then((newUser) => {
+            res.status(201).json({ message: 'utilisateur  enregistré !' });
+            // Récupérer l'_id mongo de l'utilisateur
+            console.log(newUser._id);
+        }
+
+        )
+        .catch(error => res.status(400).json({ error }));
+
 });
+
+// Signin - cet endpoint récupère uniquement les requêtes post avec app.post
+app.post('/api/auth/login', async (req, res) => {
+    try {
+        // récupérer l'email depuis le body
+        const { email } = req.body;
+
+        // vérifier que l'email est fourni
+        if (!email) {
+            return res.status(400).json({ error: "no email" });
+        };
+
+        // cherche user dans  la base 
+        const user = await User.findOne({ email });
+
+        if (user) {
+            // Si user existe
+            console.log('user is :', user);
+            return res.status(200).json(user);
+        } else {
+            // Si pas de user
+            console.log('no user in db');
+            return res.status(400).json({ error });
+        };
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error });
+    };
+});
+
 
 
 // ajouter un livre en bdd
