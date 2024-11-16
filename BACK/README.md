31946455ca0da41ae56490e04746e0684da163c4
jontehruss <johann.russeau@protonmail.com>
2024-11-16 10:54:16 +0100

    books routes (add, delete)

--- BACK/README.md
+++ BACK/README.md
@@ -7,20 +7,10 @@ npm install -g nodemon
 # installer express & middleware
 npm install express --save
 
-# installer mongoDB
-## fait avec MongoDB for Windows
-## Compass CLI
-### admin db
-* user : admin
-* password : p@ssw0rd!
-### mvgdb
-* user : mvgdbuser
-* password : p@ssw0rd!
-
-
-
-
 
 # installer mongoose pour accéder depuis le backend à la DB
 <!-- Mongoose facilite les interactions entre Express et MongoDB -->
 npm install mongoose
+
+# déporter les informations privées en variables d'environnement
+npm install dotenv
--- BACK/app.js
+++ BACK/app.js
@@ -1,77 +1,62 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const app = express();
+
+// package pour interragir avec mongo
 const mongoose = require('mongoose');
 
-
-require('dotenv').config();
-
+// body parser pour exploiter les données transmises en json
+const bodyParser = require('body-parser');
 
 //  importer les modèles de données 
 const User = require('./models/User');
 const Book = require('./models/Book');
 
-const app = express();
+//  Variables d'environnement
+require('dotenv').config();
+
 
 // connexion à la base de données 
-
-mongoose.connect( process.env.MONGODB_URI ,
+mongoose.connect(process.env.MONGODB_URI,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        dbName: 'mvgdb',
-    }
-)
+        dbName: process.env.DB_NAME,
+    })
     .then(() => console.log('connexion DB ok!'))
     .catch(err => console.log('connexion failed ! ', err));
 
+// app.use() permet d'attribuer un middleware à une route spécifique (ou toutes si non précisé en argument)
 
 // intercepter tous les content type json -> permet d'accéder au body des requêtes 
 app.use(express.json());
 
-// ajouter les Origin, Headers et méthodes autorisées pour CORS
+// ajouter les Origin, Headers et Méthodes autorisées pour CORS
 // 1er middleware -> Appliqué sur toutes les routes
 app.use((req, res, next) => {
-
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-})
+});
 
-// app.use() permet d'attribuer un middleware à une route spécifique (ou toutes si non précisé en argument)
-
-// app.get pour capturer uniquement les requêtes en méthode GET
-// app.get('/api/books', (req, res, next) => {
-//     const books = [
-//         {
-//             ...res.body
-//         }
-//     ];
-//     res.status(200).json(books);
-// });
 
 // Signin - cet endpoint récupère uniquement les requêtes post avec app.post
 app.post('/api/auth/login', (req, res, next) => {
-    console.log(req.body);
-
     // créer un instance du modèle user
     const user = new User({
         email: req.body.email,
         password: req.body.password
     });
 
-    console.log(user);
-
-    // résoudre la requête pour que ça ne plante pas !
-    //  code 201 pour la création de ressources
+    // résoudre la requête
     res.status(201).json({
         message: 'Login recieved !'
     });
-
 });
 
+
 // ajouter un livre en bdd
-app.post('/api/books', (req, res, next) => {
+app.post('/api/books', (req, res) => {
     console.log(req.body);
 
     const book = new Book({
@@ -82,18 +67,45 @@ app.post('/api/books', (req, res, next) => {
         genre: req.body.genre,
     });
 
+    // ! version simplifié par le "Spread Operator" -> fait une copie de tous les éléments de req.body
+    // const book = new Book({
+    //     ...req.body
+    // });
 
-    // méthode save pour enregistrer en base de données
+    // méthode save() pour enregistrer en base de données
     book.save()
-        .then(() => res(201).json({ message: 'livre enregistré !' }))
+        .then(() => res.status(201).json({ message: 'livre enregistré !' }))
         .catch(error => res.status(400).json({ error }));
+});
 
-    next();
+// app.get pour capturer uniquement les requêtes en méthode GET
+app.get('/api/books', (req, res) => {
 
+    // Méthode find fournie par le modèle pour requêter la base (avec des paramètres il est possible d'affiner la requête)
+    Book.find()
+        .then(books => res.status(200).json(books))
+        .catch(error => res.status(400).json({ error }));
 });
 
 
+// app.get pour capturer uniquement les requêtes en méthode GET
+app.get('/api/books/:id', (req, res) => {
 
+    // Méthode findOne sur la paramètre id de la request 
+    Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(400).json({ error }));
+});
+
+
+// app.delete pour supprimer un item en base de données
+app.delete('/api/books/:id', (req, res) => {
+
+    //  Méthode deleteOne pour cibler le avec l'id
+    Book.deleteOne({ _id: req.params.id })
+        .then(book => res.status(200).json({ message: 'livre supprimé !' }))
+        .catch(error => res.status(400).json({ error }))
+});
 
 
 module.exports = app;
